cb591a5d051f634176aae19bacd1d16abc83c2ec
Mauricio Saglietto <msaglietto@gmail.com>
2012-07-11 18:13:39 -0300

    Add test for promises
    Test when subscribe to then of a fullilled promise

--- test/bucefalo.promise.js
+++ test/bucefalo.promise.js
@@ -1,19 +1,19 @@
 var expect = require("chai").expect,
 	bucefalo = require("../dist/bucefalo-utils.module.js");
 
-describe ("Bucefalo Promise", function(){
-	
-	it('should allow me to create a fullfiled promise',function(done){
+describe("Bucefalo Promise", function() {
+
+	it('should allow me to create a fullfiled promise', function(done) {
 		var p = bucefalo.promise("Success!");
-		p.success(function(val){
+		p.success(function(val) {
 			expect(val).to.equal("Success!");
 			done();
 		});
 	});
-	
-	it('should allow me to fullfil a promise',function(done){
+
+	it('should allow me to fullfil a promise', function(done) {
 		var p = bucefalo.promise();
-		p.success(function(val){
+		p.success(function(val) {
 			expect(val).to.equal("Success!");
 			done();
 		});
@@ -21,9 +21,9 @@ describe ("Bucefalo Promise", function(){
 		p.succeed("Success!");
 	});
 
-	it('should allow me to fail a promise',function(done){
+	it('should allow me to fail a promise', function(done) {
 		var p = bucefalo.promise();
-		p.error(function(err){
+		p.error(function(err) {
 			expect(err).to.equal("Error!");
 			done();
 		});
@@ -31,42 +31,65 @@ describe ("Bucefalo Promise", function(){
 		p.fail("Error!");
 	});
 
-	it('should allow me to subscribe for success to a succedeed promise',function(done){
+	it('should allow me to subscribe for success to a succedeed promise', function(done) {
 		var p = bucefalo.promise();
-		p.success(function(val){
+		p.success(function(val) {
 			expect(val).to.equal("Success!");
 		});
 		p.succeed("Success!");
-		p.success(function(val){
+		p.success(function(val) {
 			expect(val).to.equal("Success!");
 			done();
 		});
 	});
 
-	it('should allow me to subscribe for error to a failed promise',function(done){
+	it('should allow me to subscribe for error to a failed promise', function(done) {
 		var p = bucefalo.promise();
-		p.error(function(err){
+		p.error(function(err) {
 			expect(err).to.equal("Error!");
 		});
 		p.fail("Error!");
-		p.error(function(err){
+		p.error(function(err) {
 			expect(err).to.equal("Error!");
 			done();
 		});
 	});
 
-	it('should allow me to subscribe a function if the promise succeed or fail',function(done){
+	it('should allow me to subscribe a function if the promise succeed or fail', function(done) {
 		var p = bucefalo.promise(),
 			p1 = bucefalo.promise();
-		p.then(function(val){
+		p.then(function(val) {
 			expect(val).to.equal("Error!");
 		});
-		p1.then(function(val){
+		p1.then(function(val) {
 			expect(val).to.equal("Success!");
 			done();
 		});
 		p.fail("Error!");
 		p1.succeed("Success!");
+
+	});
+
+	it('should allow me to subscribe a function to succeed or fail promises', function(done) {
+		var p = bucefalo.promise(),
+			p1 = bucefalo.promise();
+		p.then(function(val) {
+			expect(val).to.equal("Error!");
+		});
+		p1.then(function(val) {
+			expect(val).to.equal("Success!");
+		});
+		p.fail("Error!");
+		p1.succeed("Success!");
+
+		//Subscribe to fullfilled promises
+		p.then(function(val) {
+			expect(val).to.equal("Error!");
+		});
+		p1.then(function(val) {
+			expect(val).to.equal("Success!");
+			done();
+		});
 
 	});
 
